8d693bb7843fcfb0844249d9e7a828356260e744
Antonio <aruballo@uci.edu>
2015-05-05 20:11:31 -0700

    initial commit

---
+++ js/generateStudents.js
@@ -0,0 +1,11 @@
+var attendanceApp = attendanceApp || {};
+
+attendanceApp.generateStudents = function(){
+	if(!localStorage.attendance){
+		function getRandom() {
+            return (Math.random() >= 0.5);
+        }
+		
+		
+	}
+}
---
+++ js/students.js
@@ -0,0 +1,4 @@
+//List of student names
+var attendanceApp = attendanceApp || {};
+
+attendanceApp.studentNameList = ["Slippy", "Peppy", "Fox", "Falco", "Bill", "Kat", "General"];
